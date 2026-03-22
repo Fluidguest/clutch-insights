a9d02c013c0de88bebea1a28072d15fc593edd35
Fluidguest <39348975+Fluidguest@users.noreply.github.com>
2026-03-22 11:08:37 -0300

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,11 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'app.lovable.a3fc67fb3bc348aea6a02fab795f7ac3',
   appName: 'Controle Embreagem',
-  webDir: 'dist',
-  server: {
-    url: 'https://a3fc67fb-3bc3-48ae-a6a0-2fab795f7ac3.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  }
+  webDir: 'dist'
 };
 
 export default config;
